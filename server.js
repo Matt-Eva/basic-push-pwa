6848d2d1e5491925017c4458bb6d730401fcef3a
Matt Eva <matteweva@gmail.com>
2024-01-30 15:10:10 -0800

    added console.log to check publicKey on server

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const publicKey = process.env.VAPID_PUBLIC_KEY;
 const privateKey = process.env.VAPID_PRIVATE_KEY;
 const vapidSubject = process.env.VAPID_SUBJECT;
 
+console.log(publicKey);
+
 webPush.setVapidDetails(vapidSubject, publicKey, privateKey);
 
 app.post("/subscribe", (req, res) => {
