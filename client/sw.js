38f98ebed3fdd99cf69a0ef08e7eb001af04170f
Matt Eva <matteweva@gmail.com>
2024-01-31 16:40:33 -0800

    added wait until back to push event listener in sw"

--- client/sw.js
+++ client/sw.js
@@ -1,9 +1,11 @@
 self.addEventListener("push", (e) => {
   const data = e.data.json();
 
-  self.registration.showNotification(data.title, {
-    body: data.body,
-  });
+  e.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+    })
+  );
 
   self.clients.matchAll().then((clients) => {
     clients.forEach((client) => {
